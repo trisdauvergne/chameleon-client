4ba7a66b097bd19a2522590132fa387d0a84673a
Josephine Hedman <josephine.hedman@gmail.com>
2021-04-02 17:59:10 +0200

    worked on things

--- src/components/reviewfeed/ReviewFeed.js
+++ src/components/reviewfeed/ReviewFeed.js
@@ -4,7 +4,6 @@ import Review from '../review/Review.js';
 
 const ReviewFeed = ({userId}) => {
     const [reviews, setReviews] = useState(null);
-    // const [rating, setRating] = useState(null);
 
     const getReviews = async () => {
         const data = await fetch(`${host}/reviews/${userId}`);
@@ -12,20 +11,9 @@ const ReviewFeed = ({userId}) => {
         setReviews(reviewData);
     }
 
-    // const getRating = async () => {
-    //     const data = await fetch(`${host}/users/owner/${userId}`);
-    //     const ratingData = await data.json();
-    //     setRating(ratingData.rating);
-    // }
-
     useEffect(() => {
         getReviews();
-        // getRating();
     }, []);
-
-    // if (!reviews || !rating) {
-    //     return null;
-    // }
 
     if (!reviews) {
         return null;
@@ -34,7 +22,6 @@ const ReviewFeed = ({userId}) => {
     return (
         <article>
             <h2>Recieved reviews</h2>
-            {/* <h3>Average rating: {rating}</h3> */}
             {reviews.map(review => <Review key={review._id} review={review}/>)}
         </article>
     )
