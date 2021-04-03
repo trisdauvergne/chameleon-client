9932670a8f95fd999137d0e9da0a0423d21d8a4e
trisdauvergne <tristan.dauvergne@gmail.com>
2021-04-03 14:37:11 +0200

    Reconfigured config with heroku

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '';
+export const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://fierce-plains-42807.herokuapp.com/';
